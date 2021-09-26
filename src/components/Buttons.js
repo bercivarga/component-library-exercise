e84d402196fbdbaf7d2a9af4f61fc43a894d84ae
bercivarga <65171545+bercivarga@users.noreply.github.com>
2021-09-26 20:19:02 +0200

    Add state styles

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components'
+import {defaultTheme, typeScale} from "../utils";
 
 export const Button = styled.button`
+  font-style: ${typeScale.paragraph};
   border-radius: 8px;
   padding: 12px 24px;
   font-size: 16px;
@@ -8,15 +10,56 @@ export const Button = styled.button`
   box-sizing: border-box;
   cursor: pointer;
   margin-right: 16px;
+  transition: background-color .2s linear, color .2s linear;
+  
+  &:hover {
+    background-color: ${defaultTheme.primaryColorHover};
+    color: ${defaultTheme.textColorOnPrimary};
+  }
+  
+  &:active {
+    background-color: ${defaultTheme.primaryColorActive};
+    border-color: ${defaultTheme.primaryColorActive};
+    color: ${defaultTheme.primaryColor};
+  }
+  
+  &:focus {
+    outline: 3px solid ${defaultTheme.primaryColorHover}
+    outline-offset: 2px;
+  }
 `
 
 export const PrimaryButton = styled(Button)`
-  background-color: blueviolet;
-  color: white;
+  background-color: ${defaultTheme.primaryColor};
+  color: ${defaultTheme.textColorOnPrimary};
+
+  &:disabled {
+    background-color: ${defaultTheme.disabled};
+    color: ${defaultTheme.textOnDisabled};
+    cursor: not-allowed;
+  }
 `
 
 export const SecondaryButton = styled(Button)`
+  background: none;
+  color: ${defaultTheme.primaryColor};
+  border: 2px solid ${defaultTheme.primaryColor};
+
+  &:disabled {
+    background-color: transparent;
+    border-color: ${defaultTheme.disabled};
+    color: ${defaultTheme.disabled};
+    cursor: not-allowed;
+  }
+`
+export const TertiaryButton = styled(Button)`
   background-color: transparent;
-  color: darkslateblue;
-  border: 2px solid darkslateblue;
+  color: ${defaultTheme.primaryColor};
+  border: 2px solid transparent;
+
+  &:disabled {
+    background: none;
+    color: ${defaultTheme.disabled};
+    cursor: not-allowed;
+  }
 `
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, {useReducer} from "react";
 import ReactDOM from 'react-dom';
-import {PrimaryButton, SecondaryButton} from "./components/Buttons";
+import {PrimaryButton, SecondaryButton, TertiaryButton} from "./components/Buttons";
+import {GlobalStyle} from "./utils";
 
 const initialState = {count: 0};
 
@@ -10,6 +11,8 @@ function reducer(state, action) {
             return {count: state.count + 1};
         case 'DECREASE':
             return {count: state.count - 1};
+        case 'RESET':
+            return {count: 0}
         default:
             throw new Error();
     }
@@ -22,7 +25,9 @@ const App = () => {
         <div style={{gap: '16px'}}>
             <PrimaryButton onClick={() => dispatch({type: 'INCREASE'})}>Increase</PrimaryButton>
             <SecondaryButton onClick={() => dispatch({type: 'DECREASE'})}>Decrease</SecondaryButton>
+            <TertiaryButton onClick={() => dispatch({type: 'RESET'})}>Reset</TertiaryButton>
             <h2>{state.count}</h2>
+            <GlobalStyle/>
         </div>
     )
 }
